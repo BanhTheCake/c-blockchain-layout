a694554f9030acaf17b3cda28c2d54fa0ebf5758
Binhanh <binhanh080103@gmail.com>
2022-08-26 17:32:00 +0700

    fix logo Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className="px-4 sm:px-8 md:px-16 pt-16 pb-8 gradient-bg-footer text-white">
         <div className="flex flex-col md:flex-row items-center justify-between mb-12">
             <div className="w-[180px] 2xl:w-[300px] mb-8 md:mb-0 md:mr-8 flex-shrink-0 flex">
-                <img className="w-full" src={Logo} alt="logo" />
+                <img className="w-full h-full" src={Logo} alt="logo" />
             </div>
             <ul className="flex flex-wrap    w-full md:w-fit md:justify-center justify-between">
                 {['Market', 'Exchange', 'Tutorials', 'Wallets'].map((item, index) => {
